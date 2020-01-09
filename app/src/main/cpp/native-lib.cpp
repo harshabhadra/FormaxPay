c6e6674def560444f8d71aa36c7f391a504c5007
harshabhadra <harshasharkey@gmail.com>
2020-01-09 14:48:38 +0530

    bug fixes

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -7,20 +7,20 @@
 extern "C" JNIEXPORT jstring
 JNICALL
 Java_com_rechargeweb_rechargeweb_Keys_apiKey(JNIEnv *env, jobject object) {
-    std::string api_key = "5!mk516*pgh154%$#$#@$";
+    std::string api_key = "";
     return env->NewStringUTF(api_key.c_str());
 }
 
 extern "C" JNIEXPORT jstring
 JNICALL
 Java_com_rechargeweb_rechargeweb_Keys_memberId(JNIEnv *env, jobject object) {
-    std::string member_id = "3368";
+    std::string member_id = "";
     return env->NewStringUTF(member_id.c_str());
 }
 
 extern "C" JNIEXPORT jstring
 JNICALL
 Java_com_rechargeweb_rechargeweb_Keys_apiPassword(JNIEnv *env, jobject object) {
-    std::string api_password = "12345";
+    std::string api_password = "";
     return env->NewStringUTF(api_password.c_str());
 }
